760a74ba60f5a643b03d4480f983d2a21f6f90fd
welltonq <welltonq@outlook.com>
2019-08-14 17:09:04 -0300

    Configurando responsividade da página

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <title>Construtor Digital</title>
-    <!-- Corrigir depois-->
+    <!-- Corrigir depois
     <style>
         body{
             background-image: url("assets/images/grupo.png");
@@ -16,40 +16,46 @@
             background-repeat: no-repeat;
             background-color: #21566E;
         }
-    </style>
+    </style>-->
 </head>
 <body>
-    
+<div id="banner">
     <header>
         <div class="container">
             <div class="topoleft">
                 <a href="./"><img src="assets/images/Obrasnet_horizontal.png" alt="logo" id="logo"></a>
             </div>
             <div class="navmenu">
-                    <nav>
-                        <ul>
-                            <li class="active"><a href="./">Home</a></li>
-                            <li><a href="#">Planos</a></li>
-                            <li><a href="#">Blog</a></li>
-                            <li><a href="#">Contato</a></li>
-                            <li><button href="#" class="btn btn-outline-light btn-lg" id="entrar">Entrar</button></li>
-                            <li><button href="#" class="btn btn-warning btn-lg" id="cadastrar">Cadastre-se</button></li>
-                        </ul>
-                    </nav>
-            </div>
+                <a href="#" ><img src="assets/images/menu.png" width="50" alt="menu" class="menumobile" id="navmenu"></a>
+                <nav>
+                    <ul>
+                        <li class="active"><a href="./">Home</a></li>
+                        <li><a href="#">Planos</a></li>
+                        <li><a href="#">Blog</a></li>
+                        <li><a href="#">Contato</a></li>
+                        <li><button href="#" class="btn btn-outline-light btn-lg" id="entrar">Entrar</button></li>
+                        <li><button href="#" class="btn btn-warning btn-lg" id="cadastrar">Cadastre-se</button></li>
+                    </ul>
+                </nav>
+            </div> 
         </div>
     </header>
 
+
     <section>
-        <h1>A sua ferramenta online para a Gestão Financeira e de Projeto!</h1>
+        <div class="container column"> 
+            <div class="textbanner">
+                <h1>A sua ferramenta online para a Gestão Financeira e de Projetos!</h1>
+                <p>Perfeito para a gestão de pequenas empresas da construção civil. Registre-se e veja como é fácil ter controle total, relatório e indicadores em tempo real!</p>
+                
+            </div>
+            <div class="buttonbanner">
+                <button href="#" class="btn btn-warning btn-lg" id="">Experimente por 30 dias</button>
+            </div>
+        </div>
     </section>
 
-    <!--
-    <div class="container">
-        <img src="assets/images/architecture-1138967_1920.jpg" class="img-fluid">
-    </div> Fim do container-->
-
-
+</div>
     <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
     <script type="text/javascript" src="assets/js/bootstrap.bundle.min.js"></script>
     <script type="text/javascript" src="assets/js/script.js"></script>
